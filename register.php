e4b55b2da06a317dc56f7bbcebfc887c0763f34c
Miri94400 <Miri.mohammed02@gmail.com>
2025-06-24 13:28:46 +0200

    test pour syrine

--- register.php
+++ register.php
@@ -116,7 +116,8 @@ if (isset($_POST['submit'])) {
         <label for="confirm_password">Confirmer le mot de passe :</label><br>
         <input type="password" id="confirm_password" name="confirm_password" required><br><br>
         <label> syrine : </label>
-        <input type = Fille name = "syrine" value = "syrine" checked> <br><br>
+        <input type = Fille name = "syrine" value = "syrine" checked>
+        <h1> syrine <h1> <br><br>
        
         <button type="submit" name="submit">S'inscrire</button>
     </form>
